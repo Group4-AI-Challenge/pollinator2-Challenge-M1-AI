36bbc1c429ecccf05a7635e8c989756bec8a48cf
kcarka <kcarka12@gmail.com>
2026-02-02 19:54:46 +0100

    correct split.py in competition bundle

--- Competition_Bundle/split.py
+++ Competition_Bundle/split.py
@@ -2,23 +2,36 @@ import numpy as np
 
 
 
-
-#X  = np.load("data_2/X.npy")
-#y  = np.load("data_2/y.npy")
-#angles = np.load("data_2/angles.npy")
-
-#labels = np.unique(y)
-
-
-
+ 
 def get_angles(label, m):
-    # Returns the angles where a label appears
+    '''
+    Calculates the angles where a label appears
+    
+    :param label: An integer representing the label
+    :param m: The matrix containing the angles information
+    Return : An array containing the angles where the label appears
+
+    ## This function is not supposed to be used directly. Use split_data instead.
+    '''
     line = m[label]
     return np.array([i for i in range(len(line)) if line[i]])
 
 
-def split_label(label, matrix, test_size, random_state):
-    # R
+def split_label(X, y, angles,label, matrix, test_size):
+    '''
+    Splits the data for a given label into train and test masks
+    
+    :param X: An array containing the data points
+    :param y: An array containing the labels of the points
+    :param angles: An array containing the angles of each point
+    :param label: An integer representing the label to be split
+    :param matrix: The matrix containing the angles information
+    :param test_size: A float between 0 and 1 representing the proportion of data to be in the test set
+    :param random_state: The random state for shuffling the angles
+    Return : The trainMask, testMask for the label
+
+    ## This function is not supposed to be used directly. Use split_data instead.
+    '''
 
 
     angles_temp = get_angles(label, matrix)
@@ -37,12 +50,26 @@ def split_label(label, matrix, test_size, random_state):
     return trainMask, testMask
 
 
-def split_labels(labels, N, matrix, test_size, random_state):
+def split_labels(X, y,angles,labels, N, matrix, test_size):
+    '''
+    Splits the data for all labels into train and test masks
+    
+    :param X: An array containing the data points
+    :param y: An array containing the labels of the points
+    :param angles: An array containing the angles of each point
+    :param labels: An array containing the labels to be split
+    :param N: The number of data points
+    :param matrix: The matrix containing the angles information
+    :param test_size: A float between 0 and 1 representing the proportion of angles to be in the test set
+    :param random_state: The random state for shuffling the angles
+
+    ## This function is not supposed to be used directly. Use split_data instead.
+    '''
     train_mask = np.array([False for _ in range(N)])
     testMask = np.array([False for _ in range(N)])
     
     for label in labels:
-        train, test = split_label(label, matrix, test_size)
+        train, test = split_label(X, y, angles, label, matrix, test_size)
         train_mask = train_mask | train
         testMask = testMask | test
 
@@ -50,26 +77,24 @@ def split_labels(labels, N, matrix, test_size, random_state):
 
 
 
-def split_data(X, y, angles, random_state = 42, return_mask = False):
+def split_data(X, y, angles, test_size = 0.7, return_mask = False):
     '''
     Docstring for split_data
         Split X and y into X_train, X_test, y_train and y_test so that
         the angles for one given specie is different in the train and in the test.
 
-
     :param X: An array containing the data points
     :param y: An array containing the labels of the points
     :param angles : An array containing the angles of each point
     :param random_state: The state for selecting the angles
-    :param return_mask: A boolean set to False by default. If set to t
+    :param return_mask: A boolean set to False by default. 
+    If set to to True, the function returns two masks instead of the splitted data.
 
     Return : X_train, X_test, y_train, y_test if return mask is set to False
         a mask for the train and one for the test
         
+    ## This function is the main function to be used for splitting the data.
     '''
-
-
-
     unique_classes = np.sort(np.unique(y))
     matrix =  np.zeros((11, 31), dtype=int)
 
@@ -86,37 +111,9 @@ def split_data(X, y, angles, random_state = 42, return_mask = False):
     y = y[mask]
     angles = angles[mask]
 
-    train_mask, test_mask = split_labels(labels_kept, len(X), random_state)
-
+    train_mask, test_mask = split_labels(X, y, angles, labels_kept, len(X), matrix, test_size)
     if return_mask:
         return train_mask, test_mask
     else:
-        return X[train_mask], X[test_mask], y[train_mask], y[test_mask]
-
-
-
+        return X[train_mask], X[test_mask], y[train_mask], y[test_mask], angles[train_mask], angles[test_mask]
     
-
-
-'''
-
-
-X_train = np.load("X_train.npy")
-y_train = np.load("y_train.npy")
-angles_train = np.load("angles.npy")
-
-
-vals, counts = np.unique(y_train, return_counts=True)
-
-for val, c in zip(vals, counts):
-    print(f"Label {val} appeared {c} times.")
-
-'''
-
-    
-
-
-
-
-
-
